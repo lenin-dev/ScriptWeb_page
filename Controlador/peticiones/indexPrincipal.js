efef935dcc4d8de36eeac749912b43f2aad1c011
Lenin <116517200+lenin2499@users.noreply.github.com>
2023-01-11 18:51:37 -0600

    new file:   Controlador/peticiones/indexPrincipal.js 	modified:   Index.html 	modified:   Vistas/menu.html 	new file:   Vistas/slider.html 	new file:   css/Slider.css 	renamed:    imagenes/Logo.png -> imagenes/Logo/Logo.png 	new file:   imagenes/Logo/Logo_CLIN.png 	new file:   imagenes/Logo/cuadra.png 	deleted:    imagenes/LogoClin.png 	deleted:    imagenes/docs/CONTRATO DISENO DE PAGINA WEB (1).docx 	deleted:    imagenes/docs/Planeacion proyecto CLIN.xlsx 	deleted:    imagenes/docs/WhatsApp Image 2023-01-10 at 5.40.53 PM.jpeg 	deleted:    imagenes/docs/pagina.png 	new file:   imagenes/img/slider/image1.png 	new file:   imagenes/img/slider/image2.png 	new file:   imagenes/img/slider/image3.jpg 	new file:   imagenes/img/slider/image31.jpg

---
+++ Controlador/peticiones/indexPrincipal.js
@@ -0,0 +1,30 @@
+cargarSlider();
+cargarMenu();
+
+function cargarMenu() {
+    const http = new XMLHttpRequest();
+    const url = './Vistas/menu.html';
+
+    http.open('GET', url);
+    http.send();
+
+    http.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200) {
+            document.getElementById("contMenu").innerHTML = this.responseText;
+        }
+    }
+}
+
+function cargarSlider() {
+    const http = new XMLHttpRequest();
+    const url = './Vistas/slider.html';
+
+    http.open('GET', url);
+    http.send();
+
+    http.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200) {
+            document.getElementById("contSlider").innerHTML = this.responseText;
+        }
+    }
+}
