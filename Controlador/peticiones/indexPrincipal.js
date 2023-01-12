90a61e4d9dd064195000326fb3fa81c55a79afaf
Carlosvg95 <carlos.06.guzman@gmail.com>
2023-01-11 22:02:19 -0600

    modified:   Controlador/peticiones/indexPrincipal.js 	modified:   Index.html 	new file:   Vistas/QuienSomos.html 	new file:   css/QuienesSomos.css 	new file:   imagenes/mision.png 	new file:   imagenes/vision.png

--- Controlador/peticiones/indexPrincipal.js
+++ Controlador/peticiones/indexPrincipal.js
@@ -1,5 +1,6 @@
 cargarSlider();
 cargarMenu();
+cargarQuien();
 
 function cargarMenu() {
     const http = new XMLHttpRequest();
@@ -25,6 +26,20 @@ function cargarSlider() {
     http.onreadystatechange = function() {
         if(this.readyState == 4 && this.status == 200) {
             document.getElementById("contSlider").innerHTML = this.responseText;
+        }
+    }
+}
+
+function cargarQuien() {
+    const http = new XMLHttpRequest();
+    const url = '../Vistas/QuienSomos.html';
+
+    http.open('GET', url);
+    http.send();
+
+    http.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200) {
+            document.getElementById("contQuien").innerHTML = this.responseText;
         }
     }
 }
