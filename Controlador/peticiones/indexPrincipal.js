0ce1fb8caa0208f0ff9d52a4e5f08e03cb8c7c33
Lenin <116517200+lenin2499@users.noreply.github.com>
2023-02-08 15:26:12 -0600

    new file:   Controlador/peticiones/cambioMenu.js 	modified:   Controlador/peticiones/indexPrincipal.js 	deleted:    Controlador/peticiones/scrollMenu.js 	modified:   Index.html 	modified:   Vistas/Footer.html 	modified:   Vistas/QuienSomos.html 	modified:   Vistas/cotizacion.html 	modified:   Vistas/menu.html 	modified:   Vistas/ofrecemos.html 	modified:   Vistas/servicios.html 	modified:   Vistas/slider.html 	modified:   Vistas/tarjetas.html 	deleted:    css/Footer.css 	deleted:    css/MenuC.css 	deleted:    css/QuienesSomos.css 	deleted:    css/Slider.css 	deleted:    css/cotizacion.css 	new file:   css/principal.css 	deleted:    css/servicios.css

--- Controlador/peticiones/indexPrincipal.js
+++ Controlador/peticiones/indexPrincipal.js
@@ -1,3 +1,5 @@
+new WOW().init();
+
 cargarSlider();
 cargarMenu();
 cargarQuien();
--- Controlador/peticiones/scrollMenu.js
+++
@@ -1,9 +0,0 @@
-const navScroll = document.querySelector('navbar');
-
-window.addEventListener("scroll", () => {
-    if(window.scrollY >= 56) {
-        navScroll.classList.add('nav-scroll');
-    } else if(window.scrollY < 56) {
-        navScroll.classList.remove('nav-scroll');
-    }
-});
